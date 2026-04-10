0a382d5308107b71b5f3eaec0653e3f1023358e1
Brian-Advertified <Brian@advertified.com>
2026-04-10 17:24:40 +0200

    Add suburb hard-filter test

--- tests/Advertified.App.Tests/PlanningEligibilityServiceGeographyAliasTests.cs
+++ tests/Advertified.App.Tests/PlanningEligibilityServiceGeographyAliasTests.cs
@@ -38,4 +38,66 @@ public sealed class PlanningEligibilityServiceGeographyAliasTests
         var result = service.FilterEligibleCandidates(candidates, request);
         result.Candidates.Should().HaveCount(1);
     }
+
+    [Fact]
+    public void FilterEligibleCandidates_WhenSuburbsSpecified_RequiresSuburbMatchWithinRequestedCity()
+    {
+        var policyService = new PlanningPolicyService(new PlanningPolicySnapshotProvider(new PlanningPolicyOptions()));
+        var service = new PlanningEligibilityService(policyService, new TestBroadcastMasterDataService());
+        var request = new CampaignPlanningRequest
+        {
+            SelectedBudget = 100000m,
+            GeographyScope = "local",
+            Cities = new List<string> { "Johannesburg" },
+            Suburbs = new List<string> { "DiepKloof, Soweto" }
+        };
+
+        var matchingCandidate = new InventoryCandidate
+        {
+            SourceId = Guid.NewGuid(),
+            SourceType = "ooh",
+            DisplayName = "DiepKloof Billboard",
+            MediaType = "OOH",
+            City = "Johannesburg",
+            Suburb = "DiepKloof, Soweto",
+            Area = "DiepKloof, Soweto",
+            Cost = 45000m,
+            IsAvailable = true
+        };
+
+        var nonMatchingSuburbSameCity = new InventoryCandidate
+        {
+            SourceId = Guid.NewGuid(),
+            SourceType = "ooh",
+            DisplayName = "Rosebank Billboard",
+            MediaType = "OOH",
+            City = "Johannesburg",
+            Suburb = "Rosebank",
+            Area = "Rosebank",
+            Cost = 45000m,
+            IsAvailable = true
+        };
+
+        var suburbMatchWrongCity = new InventoryCandidate
+        {
+            SourceId = Guid.NewGuid(),
+            SourceType = "ooh",
+            DisplayName = "Pretoria DiepKloof Billboard",
+            MediaType = "OOH",
+            City = "Pretoria",
+            Suburb = "DiepKloof, Soweto",
+            Area = "DiepKloof, Soweto",
+            Cost = 45000m,
+            IsAvailable = true
+        };
+
+        var result = service.FilterEligibleCandidates(new List<InventoryCandidate>
+        {
+            matchingCandidate,
+            nonMatchingSuburbSameCity,
+            suburbMatchWrongCity
+        }, request);
+
+        result.Candidates.Should().ContainSingle(x => x.DisplayName == "DiepKloof Billboard");
+    }
 }
